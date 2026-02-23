53d39b1882c19066d0f8669ed4da970c8031eecb
andreiconstantinescu <andrei.constantinescu112@gmail.com>
2026-02-23 16:45:46 +0200

    Primul seminar lucrat la clasa

---
+++ Seminar1/src/ro/ase/cts/animals/Animal.java
@@ -0,0 +1,24 @@
+package ro.ase.cts.animals;
+
+public abstract class Animal {
+private String name;
+private int age;
+public abstract void eat(String mancare);
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
